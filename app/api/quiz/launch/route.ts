03e9d298b2904ae375447e836684210bc0082fbf
Jordi Dosne <f_github@jidai.fr>
2026-04-04 09:09:07 +0200

    feat: wip live config

--- app/api/quiz/launch/route.ts
+++ app/api/quiz/launch/route.ts
@@ -1,27 +1,22 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { createClient } from '@supabase/supabase-js';
+
 const supabase = createClient(
 	`https://${process.env.SUPABASE_ID}.supabase.co`,
 	process.env.SUPABASE_KEY!,
 );
 
-export async function POST(request: VercelRequest, response: VercelResponse) {
-	// auth by header, get stream name, and quiz config
+export async function POST(request: Request) {
+	const body: { stateName: string; stateData: unknown } = await request.json();
+	const { stateName, stateData } = body;
 
-	// Get quiz from id
-
-	// Join a room/topic. Can be anything except for 'realtime'.
 	const myChannel = supabase.channel(`quickiz.${process.env.CHANNEL}`);
 	await myChannel.send({
 		type: 'broadcast',
 		event: 'quiz-state',
-		payload: { message: 'Hi' },
+		payload: { stateName, stateData },
 	});
 
-	return new Response(
-		JSON.stringify({
-			message: 'ok',
-		}),
-	);
+	return Response.json({ message: 'ok' });
 }
